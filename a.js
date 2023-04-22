8551342f9da0e7abe593f5b613252fc800202cfb
Keerthiii098 <keerthibinu2507@gmail.com>
2023-04-22 10:11:15 +0530

    tracker

--- a.js
+++ a.js
@@ -1,1 +1,11 @@
-const lock = await navigator.wakeLock.request('screen');
+let wakeLock = null;
+
+const requestWakeLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen');
+    console.log('Wake Lock acquired');
+  } catch (err) {
+    console.error(`${err.name}, ${err.message}`);
+  }
+};
+requestWakeLock();
